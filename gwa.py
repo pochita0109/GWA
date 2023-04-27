4fa01b0173f09055d794ea66a97aff4459ae1510
pochita0109 <akosicosta@gmail.com>
2023-04-26 21:41:36 -0700

    Declared variables

--- gwa.py
+++ gwa.py
@@ -8,10 +8,9 @@
 
 # Create a txt file that will read a file with 20 names and GWA on it
 with open("name_gwa.txt", "r") as input_file:
-# Read the contents of names_gwa.txt
-     for line in input_file:
-         print(line.strip())
 # Declare a variable for the student with highest GWA and its GWA
+     highest_gwa_name = ""
+     ceiling_gwa = 5.00
 # Split the student's name and its corresponding GWA
 # Convert the gwa of students into float
 # If GWA is the highest, showcase the name and its GWA
