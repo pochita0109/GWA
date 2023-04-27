2020dc46540f092edc28dc4c64a04baf937cdcd4
pochita0109 <akosicosta@gmail.com>
2023-04-26 21:00:26 -0700

    First commit, Pseudocode

---
+++ gwa.py
@@ -0,0 +1,15 @@
+# Kenneth John Costa
+# Assignment 4
+# GWA (Highest)
+
+# Write a Python program that read a file containing the name of 
+# 20 students together with their GWA. The program will outputs the 
+# name of the student who got the highest GWA (including the GWA). 
+
+# Create a txt file that will read a file with 20 names and GWA on it
+# Read the contents of names_gwa.txt
+# Declare a variable for the student with highest GWA and its GWA
+# Split the student's name and its corresponding GWA
+# Convert the gwa of students into float
+# If GWA is the highest, showcase the name and its GWA
+# Print the output
